b78113d1d18841bb46fe63847d76f2d33ff60c99
A.Unger <zyxancf@gmail.com>
2021-10-21 12:55:38 +0200

    poc

---
+++ ocis/pkg/command/experimental.go
@@ -0,0 +1,96 @@
+//go:build !simple
+// +build !simple
+
+package command
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/owncloud/ocis/ocis-pkg/config"
+	"github.com/owncloud/ocis/ocis/pkg/register"
+	pcommand "github.com/owncloud/ocis/proxy/pkg/command"
+	"github.com/thejerf/suture/v4"
+	"github.com/urfave/cli/v2"
+)
+
+type serviceFuncMap map[string]func(*config.Config) suture.Service
+
+type Service struct {
+	Supervisor *suture.Supervisor
+	Services   serviceFuncMap
+	Log        log.Logger
+
+	serviceToken map[string][]suture.ServiceToken
+	context      context.Context
+	cancel       context.CancelFunc
+}
+
+// Experimental uses an opinionated experimental runtime.
+func Experimental(cfg *config.Config) *cli.Command {
+	return &cli.Command{
+		Name:     "experimental",
+		Usage:    "Start fullstack server",
+		Category: "Experimental",
+		Action: func(c *cli.Context) error {
+			var (
+				s               = Service{}
+				rootCtx, cancel = context.WithCancel(context.Background())
+			)
+			defer cancel()
+
+			// halt listens for interrupt signals and blocks.
+			halt := make(chan os.Signal, 1)
+			signal.Notify(halt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
+
+			// tolerance controls backoff cycles from the supervisor.
+			tolerance := 5
+			totalBackoff := 0
+
+			// Start creates its own supervisor. Running services under `ocis server` will create its own supervision tree.
+			s.Supervisor = suture.New("ocis", suture.Spec{
+				EventHook: func(e suture.Event) {
+					if e.Type() == suture.EventTypeBackoff {
+						totalBackoff++
+						if totalBackoff == tolerance {
+							halt <- os.Interrupt
+						}
+					}
+				},
+				FailureThreshold: 5,
+				FailureBackoff:   3 * time.Second,
+			})
+
+			// setup runtime net listener
+			l, err := net.Listen("tcp", net.JoinHostPort("localhost", "16666"))
+			if err != nil {
+				panic(err)
+			}
+
+			// add the proxy service
+			s.Supervisor.Add(pcommand.NewPSuture(rootCtx))
+
+			// run supervised services
+			go s.Supervisor.ServeBackground(s.context)
+
+			go func() {
+				<-halt
+				os.Exit(0)
+			}()
+
+			fmt.Println("runtime listening on: localhost:16666")
+			return http.Serve(l, nil)
+		},
+	}
+}
+
+func init() {
+	register.AddCommand(Experimental)
+}
---
+++ proxy/pkg/command/server_action.go
@@ -0,0 +1,74 @@
+package command
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/thejerf/suture/v4"
+
+	"github.com/owncloud/ocis/proxy/pkg/metrics"
+	"github.com/owncloud/ocis/proxy/pkg/proxy"
+	proxyHTTP "github.com/owncloud/ocis/proxy/pkg/server/http"
+
+	"github.com/oklog/run"
+	"github.com/owncloud/ocis/proxy/pkg/config"
+	"github.com/urfave/cli/v2"
+)
+
+// Action does no config parsing. It assumes the config it gets is definitive.
+func Action(cfg config.Config) func(c *cli.Context) error {
+	return func(c *cli.Context) error {
+		var (
+			g           = run.Group{}
+			ctx, cancel = context.WithCancel(context.Background())
+			logger      = NewLogger(&cfg)
+		)
+
+		defer cancel()
+
+		rp := proxy.NewMultiHostReverseProxy(proxy.Logger(logger), proxy.Config(&cfg))
+
+		server, err := proxyHTTP.Server(
+			proxyHTTP.Handler(rp),
+			proxyHTTP.Logger(logger),
+			proxyHTTP.Context(ctx),
+			proxyHTTP.Config(&cfg),
+			proxyHTTP.Metrics(metrics.New()),
+			proxyHTTP.Middlewares(loadMiddlewares(ctx, logger, &cfg)),
+		)
+
+		if err != nil {
+			logger.Error().Err(err).Str("server", "http").Msg("Failed to initialize server")
+			return err
+		}
+
+		g.Add(func() error {
+			return server.Run()
+		}, func(_ error) {
+			logger.Info().Str("server", "http").Msg("Shutting down server")
+			cancel() // calling cancel() will cause the micro-service to shut down.
+		})
+
+		return nil
+	}
+}
+
+type PSuture struct {
+	ctx context.Context
+	cfg config.Config
+}
+
+func NewPSuture(ctx context.Context) suture.Service {
+	return PSuture{
+		ctx: ctx,
+	}
+}
+
+func (s PSuture) Serve(ctx context.Context) error {
+	s.cfg.Context = ctx
+	if err := Action(s.cfg)(nil); err != nil {
+		return fmt.Errorf("you're in cahoots")
+	}
+
+	return nil
+}
