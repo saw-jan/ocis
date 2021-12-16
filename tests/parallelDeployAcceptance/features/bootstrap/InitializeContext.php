a314bcf67d1c4280b0e16e773b0deb324fc4804f
saw-jan <saw.jan.grg3e@gmail.com>
2021-12-16 09:35:46 +0545

    check

---
+++ tests/parallelDeployAcceptance/features/bootstrap/InitializeContext.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+/**
+ * ownCloud
+ *
+ * @author Sajan Gurung <sajan@jankaritech.com>
+ * @copyright Copyright (c) 2021 Sajan Gurung sajan@jankaritech.com
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License,
+ * as published by the Free Software Foundation;
+ * either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+
+require_once 'bootstrap.php';
+
+/**
+ * Initialize Context
+ */
+class InitializeContext implements Context {
+
+	/**
+	 * @var FeatureContext
+	 */
+	private FeatureContext $featureContext;
+
+	/**
+	 * @BeforeScenario
+	 *
+	 * @param BeforeScenarioScope $scope
+	 *
+	 * @return void
+	 *
+	 * @throws Exception
+	 */
+	public function setUpScenario(BeforeScenarioScope $scope): void {
+		$environment = $scope->getEnvironment();
+		$this->featureContext = $environment->getContext('FeatureContext');
+	}
+}
